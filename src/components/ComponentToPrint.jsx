27b7562518587875ab6e64b8e05af447694da297
Zeidra Firdaws <abduhsttmandala@gmail.com>
2023-07-23 15:50:40 +0700

    printout

--- src/components/ComponentToPrint.jsx
+++ src/components/ComponentToPrint.jsx
@@ -27,93 +27,92 @@ export const ComponentToPrint = React.forwardRef((props, ref) => {
         note,
         tunai,
         kembali} = props;
-    
-
     const itemqty = items.reduce((acc, cur) => {
           acc += cur.qty;
           return acc;
     }, 0);
 
-  //   function printTest() {
-  //     printJS({
-  //       printable: "invprint",
-  //       type: "html",
-  //       // style: [
-  //       //   "body {margin: 0;} h4 {margin:0}"
-  //       // ],
-  //     });
-  //  }
-
     return (
-      // <div ref={ref} className="mt-0 mb-0">
-          <div ref={ref} id="invprint">
-              <div>
-              <h2 className="logo">KYOSHI BEAUTY</h2>
-              <h2 className="address">
-                {user && user.branch.address}
-              </h2>
-              <h2 className="address">
-                 WhatsApp: {user && user.branch.phone}
-              </h2>
-              {/* <label className="kiri">==========================</label> */}
+     <div ref={ref} id="invprint">
+        <div>
+          <h2 className="logo">KYOSHI BEAUTY</h2>
+          <h2 className="address">
+              {user && user.branch.address}
+          </h2>
+          <h2 className="address">
+            Telp/WA: {user && user.branch.phone}
+          </h2>
+          <div className="mb-0 mt-0 grid grid-cols-2">
               <div className="parent">
                 <span className="childkiri">NOMOR</span>
                 <span class="childkanan">{invoiceNumber}</span>
               </div>
+          </div>
+          <div className="mb-0 mt-0 grid grid-cols-2">
               <div className="parent">
                 <span className="childkiri">Tanggal</span>
                 <span class="childkanan">{today}{'-'}{time}</span>
               </div>
-              <div className="parent">
-                <span className="childkiri">Operator</span>
-                <span class="childkanan">{user && user.name}</span>
-              </div>
-              <div className="parent">
-                <span className="childkiri">Customer</span>
-                <span class="childkanan">{customerName}</span>
-                <label className="childkiri">=========================</label>
-              </div>
-                <div className="mt-0 border-t border-black/10 py-2">
-                  {/* <div className="mb-0 mt-0 w-full grid grid-cols-2 items-end">
-                      <div className="mt-0 flex flex-col items-end">
-                        <div className="mt-0 mb-0 w-full grid grid-cols-2 justify-between">
-                          <span className="kiri">NOMOR</span>
-                          <span className="kanan">{invoiceNumber}</span>
-                        </div>
+          </div>
+          <div className="mb-0 mt-0 grid grid-cols-2">
+                      <div className="parent">
+                        <span className="childkiri">Operator</span>
+                        <span class="childkanan">{user && user.name}</span>
                       </div>
-                    <span className="kiri">Tanggal </span>
-                    <span className="kanan">{today}{'-'}{time}</span>
-                    <span className="kiri">Operator</span>
-                    <span className="kanan">{user && user.name}</span>
-                    <span className="kiri">Customer</span>
-                    <span className="kanan">{customerName}</span>
-                    <span className="kiri">Keterangan</span>
-                    <span className="kanan">{note}</span> 
-                    <label className="kiri">==========================</label>
-                  </div> */}
+          </div>
+          <div className="mb-0 mt-0 grid grid-cols-2">
+                      <div className="parent">
+                        <span className="childkiri">Customer</span>
+                        <span class="childkanan">{customerName}</span>
+                        <label className="childkiri">========================</label>
+                      </div>
+          </div>
+                <div>
+                
                     {items.map((item, index) => (
                         <div
-                          className="mt-0 flex flex-col items-end"
+                          className="parent"
                           key={index}>
                           <div> 
-                             <span className="kiriitem w-full">{index + 1}.{item.name}</span>
+                             <span className="childkiri">{index + 1}.{item.name}</span>
                             <div>
-                             <span className="kiri1">{item.qty} jasa x {item.price}</span>
+                             <span className="kiri1">{item.qty} jasa x {numberWithCommas(item.price)}</span>
+                             <span className="childkanan">{numberWithCommas(item.price * item.qty)}</span>
                             </div>
                           </div>
-                          <div className="mt-0 mb-0 w-full grid grid-cols-2 justify-between">
-                              {/* <span className="kiri">{item.qty} jasa x {item.price}</span> */}
+                          {/* <div className="mt-0 mb-0 w-full grid grid-cols-2 justify-between">
+                              <span className="kiri">{item.qty} jasa x {item.price}</span>
                               <span className="kanan1">{item.price * item.qty}</span>
-                          </div>
+                          </div> */}
                         </div>
                       ))}
-                      <label className="kiri">==========================</label>
+                      <label className="kiri">========================</label>
                   <div>
                   <div className="mb-0 mt-0 grid grid-cols-2">
-                      <span className="kiri">ITEM:</span>
-                      <span className="kanan">{itemqty}</span>
-                      <span className="kiri">DISCOUNT:</span>
-                      <span className="kanan">{discount}</span>
+                      <div className="parent">
+                        <span className="childkiri">ITEM</span>
+                        <span class="childkanan">{itemqty}</span>
+                      </div>
+                  </div>
+                  <div className="mb-0 mt-0 grid grid-cols-2">
+                      <div className="parent">
+                        <span className="childkiri">DISCOUNT</span>
+                        <span class="childkanan">{numberWithCommas(discount)}</span>
+                       </div>
+                  </div>
+                  <div className="parent">
+                      <div className="parent">
+                        <span className="childkiri">TOTAL</span>
+                        <span className="childkanan">
+                        {numberWithCommas(total) % 1 === 0
+                          ? numberWithCommas(total)
+                          : numberWithCommas(total)}
+                      </span>
+                        <label className="childkiri">========================</label>
+                      </div>
+                  </div>
+                  {/* <div>
+                      
                       <span className="kiri">TOTAL:</span>
                       <span className="kanan">
                         {numberWithCommas(total) % 1 === 0
@@ -121,7 +120,7 @@ export const ComponentToPrint = React.forwardRef((props, ref) => {
                           : numberWithCommas(total)}
                       </span>
                       <label className="kiri">==========================</label>
-                  </div>
+                  </div> */}
                     {/* <div className="flex w-full mt-0 justify-between">
                       <span className="text-xs font-merchant">TUNAI</span>
                       <span className="text-xs font-merchant">{tunai}</span>
@@ -139,8 +138,8 @@ export const ComponentToPrint = React.forwardRef((props, ref) => {
                   </div>
                 </div>
              <h2 className="address">TERIMAKASIH</h2>
-              </div>
-          </div>
+        </div>
+      </div>
       // </div>
     );
 });
